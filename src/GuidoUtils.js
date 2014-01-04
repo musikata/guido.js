d39745c43c3fab0d530b6b3ca2c049a99fb6f54c
adorsk <adorsk>
2014-01-04 18:15:07 -0500

    added math functions ot utils

--- src/GuidoUtils.js
+++ src/GuidoUtils.js
@@ -20,6 +20,26 @@ function(){
       var intPart = num - fracPart;
       return {i: intPart, f: Math.abs(fracPart)};
     },
+
+    // greatest common denominator.
+    getGreatestCommonDivisor: function(i1, i2){
+      var tmp;
+      while (i2) {
+        tmp = i2;
+        i2 = i1 % i2;
+        i1 = tmp;
+      }
+      return i1;
+    },
+
+    // smallest common multiple.
+    getSmallestCommonMultiple: function(i1, i2){
+      if (i1 == i2){
+        return i1;
+      }
+      var gcd= GuidoUtils.getGreatestCommonDivisor(i1, i2);
+      return (i1/gcd) * i2;
+    }
   };
 
   return GuidoUtils;
