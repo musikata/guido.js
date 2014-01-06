774a3e2a7b09b2058bf5894bc765dccf5badfa53
adorsk <adorsk>
2014-01-06 16:34:36 -0500

    partway through implementation of ARMusic

---
+++ test/specs/abstract/ARMusic.spec.js
@@ -0,0 +1,13 @@
+define(
+  [
+    'guido/abstract/ARMusic'
+],
+function(
+  ARMusic
+){
+  ddescribe('ARMusic', function(){
+    it('should be defined', function(){
+      expect(ARMusic).toBeDefined();
+    });
+  });
+});
